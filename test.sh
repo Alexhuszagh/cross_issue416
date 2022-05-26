beee5cf9d54508c14e3f92dbec1bf128b8687ab4
Alex Huszagh <ahuszagh@gmail.com>
2022-05-26 08:46:08 -0500

    Added working tests for Rustc 1.52.0

--- test.sh
+++ test.sh
@@ -4,9 +4,12 @@ if ! command -v cross &> /dev/null; then
     cargo install cross --git https://github.com/cross-rs/cross
 fi
 
+rustup toolchain install nightly-2021-03-25
+toolchain="rustup run nightly-2021-03-25-x86_64-unknown-linux-gnu"
+
 # Test issue #416
 # Test without LTO
-cross rustc --target armv7-unknown-linux-musleabihf --verbose
+$toolchain cross rustc --target armv7-unknown-linux-musleabihf --verbose
 
 # Test with fat LTO
-cross rustc --target armv7-unknown-linux-musleabihf --verbose --release
+$toolchain cross rustc --target armv7-unknown-linux-musleabihf --verbose --release
